138344a3ee73f0c85f2c62a911f98f0b36da1671
thomasZen <thomas.zenkel@gmail.com>
2024-06-05 15:38:05 +0200

    Use pyproject.toml instead of setup.py (#4)

--- markup_tag_evaluation/evaluate_markup_tags.py
+++ markup_tag_evaluation/evaluate_markup_tags.py
@@ -7,12 +7,12 @@ from markup_tag_evaluation.tag_evaluation import evaluate_segments
 
 
 def parse_args():
-    parser = argparse.ArgumentParser("Calculate the number of correctly placed tags (defined as <.*>)"
+    parser = argparse.ArgumentParser("Calculate the number of correctly placed tags"
                                      " based on a reference and a hypothesis.")
     parser.add_argument("reference", help="Path of the reference file including tags.")
     parser.add_argument("hypothesis", help="Path of the hypothesis file including tags.")
     parser.add_argument("--permissive", action="store_true",
-                        help="Do not throw errors for inconsistent reference and hypothesis sentences")
+                        help="Do not throw errors for inconsistent reference and hypothesis tags")
 
     return parser.parse_args()
 
@@ -22,10 +22,14 @@ def read_text(path: str) -> List[str]:
         return [" ".join(s.split()) for s in f]
 
 
-if __name__ == "__main__":
+def main():
     args = parse_args()
     reference = read_text(args.reference)
     hypothesis = read_text(args.hypothesis)
 
     tag_metric = evaluate_segments(reference, hypothesis, args.permissive)
     print(tag_metric)
+
+
+if __name__ == "__main__":
+    main()
--- scripts/test.sh
+++ scripts/test.sh
@@ -3,6 +3,6 @@
 set -e
 
 pycodestyle markup_tag_evaluation --max-line-length 100
-mypy markup_tag_evaluation scripts/
+mypy markup_tag_evaluation
 python -m doctest markup_tag_evaluation/*.py
 
