06e4e5343233f29db9528bb415f4c18a54614529
Jonas <jonas.alfie@lilt.com>
2024-08-21 08:54:37 +0000

    wip

--- markup_tag_evaluation/evaluate_markup_tags.py
+++ markup_tag_evaluation/evaluate_markup_tags.py
@@ -13,8 +13,8 @@ def parse_args():
                                      " based on a reference and a hypothesis.")
     parser.add_argument("reference", help="Path of the reference file including tags.")
     parser.add_argument("hypothesis", help="Path of the hypothesis file including tags.")
-    parser.add_argument("--source", help="Path of the source file including tags. Used \
-                        mostly for knowing the language of each sentence.")
+    parser.add_argument("--source_lang", help="Path to file containing source language.")
+    parser.add_argument("--target_lang", help="Path to file containing target language.")
     parser.add_argument("--csvout",
                         help="Path where to store a CSV with results aggregated by language.")
     parser.add_argument("--permissive", action="store_true",
@@ -40,14 +40,18 @@ def main() -> None:
     args = parse_args()
     reference = read_text(args.reference)
     hypothesis = read_text(args.hypothesis)
+
+    backup_hypothesis = None
     if args.backup_hypothesis is not None:
         backup_hypothesis: Optional[List[str]] = read_text(args.backup_hypothesis)
-    else:
-        backup_hypothesis = None
 
-    source = None
-    if args.source:
-        source = read_text(args.source)
+    source_lang = None
+    if args.source_lang:
+        source_lang = read_text(args.source_lang)
+
+    target_lang = None
+    if args.target_lang:
+        target_lang = read_text(args.target_lang)
 
     if args.use_v2_tag_format:
         extract_tags_fn = extract_positions_v2
@@ -55,25 +59,26 @@ def main() -> None:
         extract_tags_fn = extract_positions
 
     tag_metrics = evaluate_segments(
-        source,
-        reference,
-        hypothesis,
-        extract_tags_fn,
-        args.permissive,
-        args.compare_strip,
-        backup_hypothesis,
+        reference_with_tags_list=reference,
+        hypothesis_with_tags_list=hypothesis,
+        tag_extraction_function=extract_tags_fn,
+        permissive=args.permissive,
+        compare_strip=args.compare_strip,
+        source_lang=source_lang,
+        target_lang=target_lang,
+        backup_hypothesis_with_tags_list=backup_hypothesis,
     )
     metrics_by_language = defaultdict(list)
     for tag_metric in tag_metrics:
-        metrics_by_language[tag_metric.tgt_language].append(tag_metric)
+        metrics_by_language[(tag_metric.src_language, tag_metric.tgt_language)].append(tag_metric)
 
     all_metrics = TagMetrics([])
-    for language, metrics in metrics_by_language.items():
-        sum_language = sum(metrics, start=TagMetric.create_empty(tgt_language=language))
+    for language_pair, metrics in metrics_by_language.items():
+        sum_language = sum(metrics, start=TagMetric.create_empty(src_language=language_pair[0], tgt_language=language_pair[1]))
         print(sum_language)
         all_metrics.metrics.append(sum_language)
 
-    sum_all = sum(tag_metrics, start=TagMetric.create_empty(tgt_language="ALL"))
+    sum_all = sum(tag_metrics, start=TagMetric.create_empty(src_language="ALL", tgt_language="ALL"))
     print(sum_all)
     all_metrics.metrics.append(sum_all)
 
